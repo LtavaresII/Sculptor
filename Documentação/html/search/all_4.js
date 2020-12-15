ebb7b7678bd35976f5109248eafc2723626bf73f
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2020-12-15 15:33:40 -0300

    Add files via upload

--- Documentação/html/search/all_4.js
+++ Documentação/html/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['forma_12',['forma',['../class_plotter.html#a398b5e46b59e6273cff59c36ba26fd8b',1,'Plotter::forma()'],['../class_plotter.html#a716ffc91883359779a7ddb51a995314c',1,'Plotter::Forma(int forma)']]]
+  ['forma_12',['Forma',['../class_plotter.html#a716ffc91883359779a7ddb51a995314c',1,'Plotter::Forma(int forma)'],['../class_plotter.html#a398b5e46b59e6273cff59c36ba26fd8b',1,'Plotter::forma()']]]
 ];
--- Documentação/html/search/functions_1.js
+++ Documentação/html/search/functions_1.js
@@ -4,6 +4,6 @@ var searchData=
   ['clicky_81',['ClickY',['../class_plotter.html#afff9172d297c9c015bc12bcb844754bc',1,'Plotter']]],
   ['cutbox_82',['CutBox',['../class_main_window.html#a6879255e74f77f9296a620599fd5f092',1,'MainWindow::CutBox()'],['../class_sculptor.html#aa84a1b12b09e9e103fc8d78f8d1bc00f',1,'Sculptor::cutBox()']]],
   ['cutellipsoid_83',['cutEllipsoid',['../class_sculptor.html#a18d2922c111c4c13653ee07d878151ad',1,'Sculptor::cutEllipsoid()'],['../class_main_window.html#a758264e88056a340b2e6eb3413cd2175',1,'MainWindow::CutEllipsoid()']]],
-  ['cutsphere_84',['CutSphere',['../class_main_window.html#a50123cccfd40219c89794a3e3bf041c0',1,'MainWindow::CutSphere()'],['../class_sculptor.html#a67ab8c0ba5116adb8af1d01ad373ac15',1,'Sculptor::cutSphere()']]],
+  ['cutsphere_84',['cutSphere',['../class_sculptor.html#a67ab8c0ba5116adb8af1d01ad373ac15',1,'Sculptor::cutSphere()'],['../class_main_window.html#a50123cccfd40219c89794a3e3bf041c0',1,'MainWindow::CutSphere()']]],
   ['cutvoxel_85',['CutVoxel',['../class_main_window.html#a5b55902c356da3727612c708ac59569d',1,'MainWindow::CutVoxel()'],['../class_sculptor.html#ad9d714a35fc8ae16d06eb5df37c3493c',1,'Sculptor::cutVoxel()']]]
 ];
