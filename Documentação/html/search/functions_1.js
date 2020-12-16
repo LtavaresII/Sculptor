38ab85e1a65735b7541f67dd81837de76addb183
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2020-12-16 18:15:23 -0300

    Add files via upload

--- Documentação/html/search/functions_1.js
+++ Documentação/html/search/functions_1.js
@@ -5,5 +5,5 @@ var searchData=
   ['cutbox_82',['CutBox',['../class_main_window.html#a6879255e74f77f9296a620599fd5f092',1,'MainWindow::CutBox()'],['../class_sculptor.html#aa84a1b12b09e9e103fc8d78f8d1bc00f',1,'Sculptor::cutBox()']]],
   ['cutellipsoid_83',['cutEllipsoid',['../class_sculptor.html#a18d2922c111c4c13653ee07d878151ad',1,'Sculptor::cutEllipsoid()'],['../class_main_window.html#a758264e88056a340b2e6eb3413cd2175',1,'MainWindow::CutEllipsoid()']]],
   ['cutsphere_84',['cutSphere',['../class_sculptor.html#a67ab8c0ba5116adb8af1d01ad373ac15',1,'Sculptor::cutSphere()'],['../class_main_window.html#a50123cccfd40219c89794a3e3bf041c0',1,'MainWindow::CutSphere()']]],
-  ['cutvoxel_85',['CutVoxel',['../class_main_window.html#a5b55902c356da3727612c708ac59569d',1,'MainWindow::CutVoxel()'],['../class_sculptor.html#ad9d714a35fc8ae16d06eb5df37c3493c',1,'Sculptor::cutVoxel()']]]
+  ['cutvoxel_85',['cutVoxel',['../class_sculptor.html#ad9d714a35fc8ae16d06eb5df37c3493c',1,'Sculptor::cutVoxel()'],['../class_main_window.html#a5b55902c356da3727612c708ac59569d',1,'MainWindow::CutVoxel()']]]
 ];
